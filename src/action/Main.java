b0eaa5bfedde16faa85f262d33b3b48d867524fe
Rizky Ardi Ilhami <rizkyardi.ilhami06@gmail.com>
2022-02-18 14:12:00 +0700

    Choose table first & implement SoC

--- src/action/Main.java
+++ src/action/Main.java
@@ -19,71 +19,85 @@ import tools.DbConnection;
  */
 public class Main {
 
-    private static long id;
-    private static int count;
-    private static String name;
-    
-    private static Dao<Region> regionDao;
-    
+    private static Action tableAction;
+
     public static void main(String[] args) {
         DbConnection connection = new DbConnection();
         System.out.println(connection.getConnection());
 
-        regionDao = new RegionDao(connection.getConnection());
-
         boolean stop = false;
-        
+        boolean stopMethod = false;
+
         Scanner obj = new Scanner(System.in);
-        Scanner objStr = new Scanner(System.in);
 
         while (!stop) {
-            System.out.println("Choose method");
-            System.out.println("(1) Show\n"
-                    + "(2) Insert\n"
-                    + "(3) Update\n"
-                    + "(4) Delete\n"
-                    + "(5) Change table\n"
-                    + "(6) Exit");
+            System.out.println("Choose table menu");
+            System.out.println("(1) Region\n"
+                    + "(2) Country\n"
+                    + "(3) Location\n"
+                    + "(4) Department\n"
+                    + "(5) Job\n"
+                    + "(6) Employee\n"
+                    + "(7) Exit");
 
-            int method = obj.nextInt();
+            int table = obj.nextInt();
 
-            switch (method) {
-                case 1: {
-                    regionDao.getAll().forEach(region -> System.out.println(
-                        region.getRegionId() + " | " + region.getRegionName() + " | " + region.getCount()
-                    ));
+            switch (table) {
+                case 1:
+                    tableAction = new RegionAction();
                     break;
-                }
-                case 2: {
-                    name = objStr.nextLine();
-                    count = obj.nextInt();
-                    regionDao.save(new Region(name, count));
-                    break;
-                }
-                case 3: {
-                    id = obj.nextLong();
-                    Region region = getRegion(id);
-                    name = objStr.nextLine();
-                    count = obj.nextInt();
-                    regionDao.update(region, new String[]{name, String.valueOf(count)});
-                    break;
-                }
-                case 4: {
-                    id = obj.nextLong();
-                    Region region = getRegion(id);
-                    regionDao.delete(region);
-                    break;
-                }
-                case 6: {
+                case 7: {
                     stop = true;
                     break;
                 }
+                default:
+                    System.out.println("Method doesn't exist!");
+                    break;
+            }
+
+            while (!stopMethod) {
+                System.out.println("Choose method menu");
+                System.out.println("(1) Show\n"
+                        + "(2) Insert\n"
+                        + "(3) Update\n"
+                        + "(4) Delete\n"
+                        + "(5) Change table\n"
+                        + "(6) Exit");
+
+                int method = obj.nextInt();
+
+                switch (method) {
+                    case 1: {
+                        tableAction.show();
+                        break;
+                    }
+                    case 2: {
+                        tableAction.insert();
+                        break;
+                    }
+                    case 3: {
+                        tableAction.update();
+                        break;
+                    }
+                    case 4: {
+                        tableAction.delete();
+                        break;
+                    }
+                    case 5: {
+                        stopMethod = true;
+                        break;
+                    }
+                    case 6: {
+                        stop = true;
+                        stopMethod = true;
+                        break;
+                    }
+                    default: {
+                        System.out.println("Method doesn't exist!");
+                        break;
+                    }
+                }
             }
         }
-    }
-    
-    private static Region getRegion(long id) {
-        Optional<Region> region = regionDao.get(id);
-        return region.orElseGet(() -> new Region("non-existing region", 0));
     }
 }
